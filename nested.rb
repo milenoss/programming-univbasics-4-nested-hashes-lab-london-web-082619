b0662b904fd78d1620362b2d91f28753e54a9ca6
Milen Khanal <milenkhanal@gmail.com>
2019-08-06 21:15:05 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,8 +1,8 @@
 
 def hopper
-	programmer_hash = 
+	programmer_hash =
  		{
-        :grace_hopper => {
+        :grace_hopper =>{
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
@@ -15,15 +15,14 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -32,16 +31,18 @@ def alan_kay_is_known_for
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
+        
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
@@ -65,7 +68,12 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+		{
+:yukihiro_matsumoto => {
+ :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+},
+
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -101,8 +109,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+   
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 
 end
