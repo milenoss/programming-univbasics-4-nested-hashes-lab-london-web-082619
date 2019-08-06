fb301e819f25c9d7c5ca3fc4b72fdfcce181a154
Milen Khanal <milenkhanal@gmail.com>
2019-08-06 21:13:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,7 @@ def changing_alan
      alans_new_info = "GUI"
    
     programmer_hash[:alan_kay][:known_for] = "GUI"
-     programmer_hash
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -130,5 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash
 
 end
